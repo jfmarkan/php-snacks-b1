14235d5575c09e510a30f552dd5ace358924ece3
jfmarkan <jfmarkan@gmail.com>
2023-07-04 12:44:05 +0200

    done

---
+++ snack4/index.php
@@ -0,0 +1,13 @@
+<?php 
+        $numMin = 4;
+        $numMax = 8;
+
+        $array=[];
+
+    for ($i=$numMin; $i < $numMax ; $i++) { 
+        array_push($array, $i);
+    }
+
+    var_dump($array);
+
+?>
